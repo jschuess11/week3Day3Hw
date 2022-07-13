204e2a3e194e4ba61b177164f60b42c6a7d78b4e
jschuess11 <108285498+jschuess11@users.noreply.github.com>
2022-07-13 18:23:16 -0400

    w3d3hw

---
+++ app.js
@@ -0,0 +1,29 @@
+const a = 2;
+const b = 4;
+const c = 6;
+const d = 8;
+const e = 10;
+const f = 1;
+const g = 3;
+const h = 5;
+const i = 7;
+const j = 9;
+
+console.log(a + j);
+console.log(c + d);
+
+console.log(b - a);
+console.log(e - b);
+
+console.log(a * b);
+console.log(j * k);
+
+console.log(h / i);
+console.log(e / g);
+
+j++;
+b++;
+
+c--;
+a--;
+
